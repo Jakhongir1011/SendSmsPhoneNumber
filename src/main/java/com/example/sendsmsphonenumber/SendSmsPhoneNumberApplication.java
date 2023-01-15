f3a48e386412052677b2a26f7fb9037dbd38253d
Jakhongir1011 <jaxongir10112000@gmail.com>
2023-01-14 23:10:54 -0800

    firstCommit

---
+++ src/main/java/com/example/sendsmsphonenumber/SendSmsPhoneNumberApplication.java
@@ -0,0 +1,13 @@
+package com.example.sendsmsphonenumber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendSmsPhoneNumberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SendSmsPhoneNumberApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sendsmsphonenumber/SendSmsPhoneNumberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sendsmsphonenumber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendSmsPhoneNumberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
